f4c22b5cfa2e2180721f9677bf9d40ce416e2e8d
JakobRechberger <jakob.rechberger@gmx.de>
2022-02-19 19:09:34 +0100

    Frontend eig fertig

---
+++ frontend/src/lib/feature/list/list.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { List } from 'src/lib/data-access/models/list';
+import { BackendService } from 'src/lib/data-access/service/backend.service';
+@Component({
+  selector: 'list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent{
+  @Input() list?: List;
+  @Input() selectedOnCreate: boolean;
+  @Output() deleteListEvent = new EventEmitter<number>();
+
+
+  showDeleteButton:boolean = false;
+
+  constructor(private backendService: BackendService) {}
+  @ViewChild('myInput') myInput: ElementRef;
+
+
+
+ngAfterViewInit(){
+      this.myInput.nativeElement.focus();
+  }
+ onMouseEnter(){
+   this.showDeleteButton = true;
+ }
+ onMouseLeave(){
+   this.showDeleteButton = false;
+ }
+ onDeleteList(){
+   this.deleteListEvent.emit(this.list.id);
+ }
+  editValue(event: any): void {
+    this.list.name = event.target.value;
+    this.backendService.updateList(this.list).subscribe();
+  }
+}
